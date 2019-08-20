e15905a19680e30aafd72f5510db1b7d316c11a4
kiranqureshi1 <kiranqureshi@outlook.com>
2019-08-20 14:10:44 +0100

    my first mini project in c sharp ever yohoo

---
+++ src/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+//webHostBuilder comes from AspNetCore.Hosting
+
+namespace src
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            var host = new WebHostBuilder()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseKestrel()
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .ConfigureKestrel((context, options) =>
+        {
+            // Set properties and call methods on options
+        })
+        .Build();
+
+    host.Run();
+        }
+    }
+}
